6f4d411944fcb72e10529ea9f56507d6d39612c5
josegonz7 <jgoncre1412@g.educaand.es>
2025-12-16 14:38:50 +0100

    ,,,

---
+++ UD04/src/ejemplos/Diapositiva13.java
@@ -0,0 +1,15 @@
+package ejemplos;
+
+public class Diapositiva13 {
+
+	public static void main(String[] args) {
+		int [] enteros = new int [5];
+		double [] doubles = new double [5];
+		boolean [] booleans = new boolean [5];
+		
+		System.out.println(enteros);
+		System.out.println(doubles);
+		System.out.println(booleans);
+	}
+
+}
