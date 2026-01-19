47ff7a31496c33e5d016ae413bd382d471d1d0f0
josegonz7 <jgoncre1412@g.educaand.es>
2026-01-19 23:28:33 +0100

    main

--- UD04/src/ejercicios/Tarea1.java
+++ UD04/src/ejercicios/Tarea1.java
@@ -1,67 +1,60 @@
 package ejercicios;
 
-
 import java.util.Random;
 
 public class Tarea1 {
-
-	public class FuncionesArrayInt {
-		
-	    /*
-	     * generaArrayInt: Genera un array de tamaño
-		n con números aleatorios cuyo intervalo
- 		(mínimo y máximo) se indica como parámetro.
-	     */
-		
-	    public static int[] generaArrayInt(int n, int minimo, int maximo) {
-	        Random rand = new Random();
-	        int[] array = new int[n];
-	        for (int i = 0; i < n; i++) {
-	            array[i] = rand.nextInt(maximo - minimo + 1) + minimo;
-	        }
-	        return array;
-	    }
-	    
-	    /*
-	     * minimoArrayInt: Devuelve el mínimo del
-	     *  array que se pasa como parámetro.
-	     */
-	    
-	    public static int minimoArrayInt(int[] array) {
-	        if (array.length == 0) throw new IllegalArgumentException("Array vacío");
-	        int minimo = array[0];
-	        for (int i = 1; i < array.length; i++) {
-	            if (array[i] < minimo) minimo = array[i];
-	        }
-	        return minimo;
-	    }
-	    
-	    /*
-	     * maximoArrayInt: Devuelve el máximo del 
-	     * array que se pasa como parámetro.
-	     */
-	    
-	    public static int maximoArrayInt(int[] array) {
-	        if (array.length == 0) throw new IllegalArgumentException("Array vacío");
-	        int maximo = array[0];
-	        for (int i = 1; i < array.length; i++) {
-	            if (array[i] > maximo) maximo = array[i];
-	        }
-	        return maximo;
-	    }
-	    
-	    /*
-	     * mediaArrayInt: Devuelve la media del
-	     *  array que se pasa como parámetro.
-	     */
-	    
-	    public static double mediaArrayInt(int[] array) {
-	        if (array.length == 0) throw new IllegalArgumentException("Array vacío");
-	        int suma = 0;
-	        for (int num : array) suma += num;
-	        return (double) suma / array.length;
-	    }
-	}
-
+    /*
+     * generaArrayInt: Genera un array de tamaño n 
+     * con números aleatorios cuyo intervalo 
+     * (mínimo y máximo) se indica como parámetro.
+     */
+	
+    public static int[] generaArrayInt(int tam, int min, int max) {
+        int[] array = new int[tam];
+        Random r = new Random();
+        for(int i = 0; i < tam; i++) {
+            array[i] = r.nextInt(max - min + 1) + min;
+        }
+        return array;
+    }
+    /*
+     * minimoArrayInt: Devuelve
+     *  el mínimo del 
+     *  array que se pasa como parámetro.
+     */
+    public static int minimoArrayInt(int[] array) {
+        int minimo = array[0];
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] < minimo) {
+                minimo = array[i];
+            }
+        }
+        return minimo;
+    }
+    /*
+     * maximoArrayInt: Devuelve el
+     *  máximo del array
+     *   que se pasa como parámetro.
+     */
+    public static int maximoArrayInt(int[] array) {
+        int maximo = array[0];
+        for(int i = 1; i < array.length; i++) {
+            if(array[i] > maximo) {
+                maximo = array[i];
+            }
+        }
+        return maximo;
+    }
+    /*
+     * mediaArrayInt: Devuelve la 
+     * media del array que 
+     * se pasa como parámetro
+     */
+    public static double mediaArrayInt(int[] array) {
+        double suma = 0;
+        for(int i = 0; i < array.length; i++) {
+            suma += array[i];
+        }
+        return suma / array.length;
+    }
 }
-
